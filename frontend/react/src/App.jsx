6035aeb2a304a6eeef32b5a9bbbc32fad15c1ad5
ABdou2365 <abdo.moutawakkil2002@gmail.com>
2024-06-21 01:28:18 +0100

    creating the card ui and map the customers

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,12 +1,65 @@
-import { Button } from '@chakra-ui/react'
+import { Text} from '@chakra-ui/react'
 import SidebarWithHeader from "./shared/SideBar.jsx";
+import {useEffect, useState} from "react";
+import {getCustomers} from "./services/client.js";
+import { Stack , Spinner } from "@chakra-ui/react";
+import Card from "./components/Card.jsx";
+import { Wrap, WrapItem } from '@chakra-ui/react'
 
 function App() {
 
-  return (
-      <SidebarWithHeader>
-          <Button colorScheme='teal' variant='outline'>Click me</Button>
-      </SidebarWithHeader>
-  )
-}
+    const [customers,setCustomers] = useState([]);
+    const [loading,setLoading] = useState(true);
+
+
+    useEffect(() => {
+            setLoading(true)
+            getCustomers().then(res => {
+                    setCustomers(res.data)
+                console.log(res.data)
+                }
+            ).catch(err => {
+                throw err;
+            }).finally(
+                () => {
+                    setLoading(false)
+                }
+                );
+        },[]
+    )
+
+        if (loading) {
+            return (
+                <SidebarWithHeader>
+                    <Stack direction='row'  spacing={4}>
+                        <Spinner size='xl'/>
+                    </Stack>
+                </SidebarWithHeader>
+            )
+        }
+
+        if (customers.length <= 0) {
+            return (
+                <SidebarWithHeader>
+                    <Text>No customer is available</Text>
+                </SidebarWithHeader>
+            )
+        }
+
+
+        return (
+            <SidebarWithHeader>
+                <Wrap spacing='30px' justify='center'>
+                    {
+                        customers.map((customer, index) => (
+                            <WrapItem key={index}>
+                                <Card {...customer} />
+                            </WrapItem>
+
+                        ))
+                    }
+                </Wrap>
+            </SidebarWithHeader>
+        )
+    }
 export default App

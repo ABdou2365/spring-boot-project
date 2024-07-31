c8e7522eaf197d9d789c735495e13e968ae4bcd3
ABdou2365 <abdo.moutawakkil2002@gmail.com>
2024-07-31 22:47:52 +0100

    expose health and infos metrics

--- backend/src/main/java/com/abde/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/abde/security/SecurityFilterChainConfig.java
@@ -37,7 +37,9 @@ public class SecurityFilterChainConfig {
                         .requestMatchers(HttpMethod.POST,"/api/v1/customers","/api/v1/auth/login")
                         .permitAll()
                         .requestMatchers(HttpMethod.GET,"/ping").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/actuator/**").permitAll()
                         .anyRequest().authenticated()
+
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
